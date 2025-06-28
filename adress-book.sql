d6900024377aa4f1dae7c02889326d62efc412b5
Abdullah Siddiqui <abdullahsidzz333@gmail.com>
2025-06-28 16:37:19 +0500

    push

--- adress-book.sql
+++ adress-book.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2025 at 09:07 PM
+-- Generation Time: Jun 28, 2025 at 01:36 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ecommerce_website`
+-- Database: `adress-book`
 --
 
 -- --------------------------------------------------------
